48fd17f2f980330113e337e1e4a87cd68581c829
Francopinedo <francopinedo23@gmail.com>
2026-01-03 22:34:24 -0300

    Configuración para exportación estática

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',      // <--- AÑADE ESTO (Genera archivos estáticos)
   images: {
+    unoptimized: true,   // <--- AÑADE ESTO (GitHub no soporta el optimizador de Next)
     domains: [],
   },
+  // basePath: '/UshuaiaBikeRental', // <--- Descomenta esto si las imágenes no cargan después
 }
 
 module.exports = nextConfig
